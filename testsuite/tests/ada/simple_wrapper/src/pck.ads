ecf874a190d7637d98601658fde5059e89f69bf6
Quentin Ochem <ochem@adacore.com>
2020-06-07 23:40:55 -0400

    Started implementation of Ada wrappers.

---
+++ testsuite/tests/ada/simple_wrapper/src/pck.ads
@@ -0,0 +1,9 @@
+package Pck is
+
+   procedure Proc1Suffix;
+
+   procedure Proc2Suffix;
+
+   procedure SomethingElse;
+
+end Pck;
---
+++ testsuite/tests/ada/simple_wrapper/test.sh
@@ -0,0 +1,2 @@
+cd out
+UWRAP "-l ada -I$ROOT/include/ -w `pwd`/../test.wrp -P `pwd`/../src/p.gpr `pwd`/../src/pck.ads"
