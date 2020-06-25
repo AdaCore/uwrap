e26e11cf6619a4ea391b8a6f13bba19df6244b2a
Quentin Ochem <ochem@adacore.com>
2020-06-25 09:05:17 -0400

    Removed files added by mistake.

--- testsuite/tests/fdump-ada-spec/error_code_into_exception/expected/test.ads
+++
@@ -1,8 +0,0 @@
-with Interfaces.C;
-with Interfaces.C.Strings; use Interfaces.C;
-package Test is
-   procedure S1 (P1 : String; P2 : String);
-   function S2 return String;
-   function S3 (Arg1 : String; Arg2 : String) return String;
-   procedure S4 (P1 : String; Leave_Me_Alone : Interfaces.C.Strings.chars_ptr);
-end Test;
