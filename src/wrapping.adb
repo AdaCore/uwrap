70077ad33923c73e875f33baa9b0922ad85b1b03
Quentin Ochem <ochem@adacore.com>
2020-09-04 21:51:46 -0400

    Added copyright notices.

--- src/wrapping.adb
+++ src/wrapping.adb
@@ -1,3 +1,22 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                                  UWrap                                   --
+--                                                                          --
+--                       Copyright (C) 2020, AdaCore                        --
+--                                                                          --
+-- UWrap is free software;  you can  redistribute it  and/or modify it      --
+-- under terms of the  GNU General Public License as published  by the Free --
+-- Software  Foundation;  either version 3,  or (at your option) any later  --
+-- version.  UWrap is distributed in the hope that it will be useful, but   --
+-- WITHOUT ANY WARRANTY;  without even the  implied warranty of MERCHANTA-  --
+-- BILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public  --
+-- License for  more details.  You should have  received  a copy of the GNU --
+-- General Public License  distributed with SVD2Ada; see file COPYING3.  If --
+-- not, go to http://www.gnu.org/licenses for a complete copy of the        --
+-- license.                                                                 --
+--                                                                          --
+------------------------------------------------------------------------------
+
 with Ada.Containers.Vectors;     use Ada.Containers;
 with Ada.Directories;
 with Ada.Strings.Unbounded;      use Ada.Strings.Unbounded;
