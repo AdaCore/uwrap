96381636060df882f23f0a897595b6633f46169d
QuentinOchem <ochem@adacore.com>
2020-09-25 14:12:09 -0400

    remved now OBE package

--- src/wrapping-utils.ads
+++ src/wrapping-utils.ads
@@ -18,7 +18,6 @@
 ------------------------------------------------------------------------------
 
 with Ada.Containers.Indefinite_Vectors;
-with Ada.Containers.Vectors;
 with Ada.Containers.Indefinite_Ordered_Maps;
 with Ada.Containers.Indefinite_Ordered_Sets;
 
@@ -30,8 +29,6 @@ package Wrapping.Utils is
    type Text_Maps_Access is access all Text_Maps.Map;
 
    package Text_Sets is new Ada.Containers.Indefinite_Ordered_Sets (Text_Type);
-
-   package Integer_Vector is new Ada.Containers.Vectors (Positive, Integer);
 
    function Remove_Quotes (Text : Text_Type) return Text_Type;
    --  Remove the quotes before and after the Text, taking into account both
