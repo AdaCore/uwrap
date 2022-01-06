4f1d9e23f3c8f0c47f3d86c8bd8d919eeb85a3ee
QuentinOchem <ochem@adacore.com>
2022-01-06 17:26:42 -0500

    improved error message

--- src/wrapping-run.adb
+++ src/wrapping-run.adb
@@ -18,6 +18,7 @@
 ------------------------------------------------------------------------------
 
 with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Characters.Conversions; use Ada.Characters.Conversions;
 with GNAT.OS_Lib; use GNAT.OS_Lib;
 
 with GNAT.Directory_Operations; use GNAT.Directory_Operations;
@@ -113,7 +114,9 @@ package body Wrapping.Run is
             Close (Dir);
          exception
             when GNAT.Directory_Operations.Directory_Error =>
-               Warning ("Cannot find the uwrap runtime");
+               Warning
+                 ("Cannot find the uwrap runtime in path "
+                  & To_Wide_Wide_String (Dir_Path));
          end Analyze_Directory;
 
       begin
